d5bf447393ba5f5837aef3659b245fe83439f0f1
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-08-22 11:05:20 -0400

    Create Login.vue

---
+++ Frontend/src/pages/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row
+                    align="center"
+                    justify="center"
+                >
+                    <v-col
+                        cols="12"
+                        sm="8"
+                        md="4"
+                    >
+                        <v-card>
+                            
+                        </v-card>
+
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+
+
+
+    </v-app>
+</template>
