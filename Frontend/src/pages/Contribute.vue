6fb6138783b8f8473bbd556c2ce1f6ddf00fbf85
Nick Liu <nickliu02@gmail.com>
2020-08-22 11:17:11 -0400

    lol

---
+++ Frontend/src/pages/Contribute.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+      <h1>Contribute</h1>
+  </v-app>
+</template>
+
+<script>
+export default {
+    name: "Contribute"
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 import Play from "../pages/Play.vue";
 import Router from "vue-router";
+import Profile from "../pages/Profile.vue";
+import Contribute from "../pages/Contribute.vue";
 
 Vue.use(Router);
 
@@ -12,6 +14,8 @@ export default new Router({
     {path: "/login", name: "Login", component: Login},
     {path: "/register", name: "Register", component: Register},
     {path: "/play", name: "Play", component: Play},
+    {path: "/profile", name: "Profile", component: Profile},
+    {path: "/contribute", name: "Contribute", component: Contribute},
   ],
   mode: "history",
   linkExactActiveClass: "active",
