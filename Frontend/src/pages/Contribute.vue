d217bbf10a8bff43bc3e60b8537e5ea2b0c5d6d7
Nick Liu <nickliu02@gmail.com>
2020-08-22 14:09:52 -0400

    lol

--- Frontend/src/pages/Contribute.vue
+++ Frontend/src/pages/Contribute.vue
@@ -14,8 +14,5 @@ export default {
 </script>
 
 <style scoped>
-  #ligma {
-    background-color: red;
-    height:1000000px;
-  }
+  
 </style>
--- Frontend/src/pages/Play.vue
+++ Frontend/src/pages/Play.vue
@@ -1,67 +1,105 @@
 <template>
   <v-app>
-      <h1 id="title">Quizzikal</h1>
+      <div id="wrapper">
+        <h1 id="title">Quizzikal</h1>
 
-      <div id="buttons">
-        <div>
-          <v-btn depressed x-large color="accent">New Challenge</v-btn>
+        <div id="buttons">
+          <div>
+            <v-btn depressed x-large color="accent">New Challenge</v-btn>
+          </div>
+
+          <div>
+            <v-btn depressed x-large color="accent">Solo Play</v-btn>
+          </div>
+          
         </div>
 
-        <div>
-          <v-btn depressed x-large color="accent">Solo Play</v-btn>
-        </div>
-        
-      </div>
 
-      
-
-
-      <div id="challenges">
-        <v-card
-          class="mx-auto"
-          max-width="400"
-        >
-          <v-card-title class="white--text black">
-            Challenges
-
-            <v-spacer></v-spacer>
-          </v-card-title>
-
-          <v-virtual-scroll
-            :items="challenges"
-            :item-height="50"
-            height="300"
+        <div id="challenges">
+          <v-card
+            class="mx-auto"
             width="200"
-            
-          > 
+          >
+            <v-card-title class="white--text black">
+              Challenges
 
-          <template v-slot="{ item }">
-            <v-list-item>
+              <v-spacer></v-spacer>
+            </v-card-title>
 
-              <v-list-item-content>
-                <v-list-item-title>{{ item.player }}</v-list-item-title>
-                
-              </v-list-item-content>
+            <v-virtual-scroll
+              :items="challenges"
+              :item-height="50"
+              height="300"
+              width="200"
+              
+            > 
 
-              <v-list-item-action>
+            <template v-slot="{ item }">
+              <v-list-item>
+
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.player }}</v-list-item-title>
+                  
+                </v-list-item-content>
+
+                <v-list-item-action>
+                  <v-btn
+                    depressed
+                    small
+                    @click.stop="openModal(item)"
+                  >
+                    View
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </template>
+
+            </v-virtual-scroll>
+          </v-card>
+
+          <v-dialog
+            v-model="isModalOpen"
+            max-width="350"
+          >
+    
+            <v-card id="modal">
+              <v-card-title class="headline justify-center">Challenge from {{ modalInfo.player }}</v-card-title>
+              
+              <v-card-text class="text-center">
+                Categories: {{ printCategories(modalInfo.categories) }}
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
                 <v-btn
-                  depressed
-                  small
+                  color="red darken-1"
+                  text
+                  @click="isModalOpen = false"
                 >
-                  View
+                    Decline
                 </v-btn>
-              </v-list-item-action>
-            </v-list-item>
-          </template>
 
-          </v-virtual-scroll>
-        </v-card>
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="isModalOpen = false"
+                >
+                    Accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
 
+             
+          </v-dialog>
+
+        </div>
       </div>
   </v-app>    
 </template>
 
 <script>
+import { mdiDna, mdiFlask, mdiMathIntegralBox, mdiBookOpenBlankVariant, mdiAtom, mdiPillar  } from '@mdi/js';
+
 export default {
   data() {
     return {
@@ -73,24 +111,37 @@ export default {
         { player: "lolxd", categories: ["Biology", "Physics"] },
         { player: "lolxd", categories: ["Biology", "Physics"] },
         { player: "ayylmao", categories: ["History", "Math"] },
-      ]
+      ],
+
+      isModalOpen: false,
+      modalInfo: { player: "", categories: [] },
     }
   },
 
   methods: {
-    printCategories(array) {
+    printCategories(categories) {
       let out = "";
-      for (let catergory in array) {
-        console.log(catergory);
-        out += catergory + " ";
+      for (let i = 0; i < categories.length; i++) {
+        out += categories[i] + ", ";
       }
-      return out;
+      return out.substring(0, out.length-2);
+    },
+
+    openModal(info) {
+      this.isModalOpen = true;
+      this.modalInfo = info;
     }
   }
 }
 </script>
 
 <style scoped>
+#wrapper {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+
 #title {
   font-size: 60px;
   text-align: center;
@@ -108,5 +159,6 @@ export default {
 #challenges {
   margin: 35px auto;
 }
+
 
 </style>
