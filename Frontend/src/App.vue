3ae60416e7004ebcf5ce9916d0a6c6389f0564f5
Nick Liu <nickliu02@gmail.com>
2020-08-22 11:18:02 -0400

    Update App.vue

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -32,7 +32,7 @@
             </v-list-item-icon>
 
             <v-list-item-content>
-              <router-link v-bind:to="item.route"><v-list-item-title>{{ item.title }}</v-list-item-title></router-link>
+              <v-list-item-title><router-link v-bind:to="item.route">{{ item.title }}</router-link></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list>
