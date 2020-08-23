d8bb3e40887980ab51081ed86b7dd45d7010465e
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-08-23 16:15:26 -0400

    setting up node

--- igni/src/routes/contribute.router.ts
+++ igni/src/routes/contribute.router.ts
@@ -4,6 +4,7 @@ import {check_auth} from "./middleware/check-auth";
 
 export const contributeRouter= express.Router();
 contributeRouter.post('create',check_auth,(req, res) => {
-    create_question(req.body.question,req.body.answer,req.body.wrong_answers)
+    create_question(req.body.question,req.body.answer,req.body.wrong_answers);
+    res.send("accepted");
 
 });
