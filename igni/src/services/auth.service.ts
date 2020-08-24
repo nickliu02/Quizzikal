cf1ce8336d208187631239d96cab3074efa0cbca
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-08-24 03:11:57 -0400

    more endpoint fixes

--- igni/src/services/auth.service.ts
+++ igni/src/services/auth.service.ts
@@ -3,9 +3,9 @@ import { jwtKey } from '../config';
 
 const jwt = require('jsonwebtoken');
 
-export const register_user = (username: string, password: string): Promise<string> => client.query(
-    'INSERT INTO users(username, password) VALUES($1, $2) RETURNING username',
-    [username, password]
+export const register_user = (username: string, password: string, real_name: string): Promise<string> => client.query(
+    'INSERT INTO users(username, password, real_name) VALUES($1, $2, $3) RETURNING username',
+    [username, password, real_name]
 )
     .then(res => generateToken(res.rows[0].username))
     .catch(e => e);
